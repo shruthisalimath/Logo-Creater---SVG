d8936f47247ec128c5380d2f87749aceac34b3c6
shru <shruthishekar@gmail.com>
2023-05-01 18:30:26 -0600

    test passed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const shapes = require('./lib/shapes.js');
+const {Triangle, Circle, Square} = require('./lib/shapes');
 
 const questions = [
     {
@@ -17,14 +17,14 @@ const questions = [
 
     },
     {
-        type: list,
+        type: 'list',
         name :'shapeType',
         message : 'Select shape of the logo',
-        choices : ['circle','square','triangle'],
+        choices : ['Circle','Square','Triangle'],
 
     },
     {
-        type: input,
+        type: 'input',
         name :'shapeColor',
         message : 'Enter color keyword  or a hexadecimal number of the text',
 
@@ -33,13 +33,44 @@ const questions = [
 ];
 inquirer.prompt(questions)
 .then((response) => {
-    fs.writeFile('logo.svg',shapes(response.text,response.textColor,response.shapeType,response.shapeColor),(err) => {
-        if(err) {
-            console.log(err);
-        }
-        else {
-            console.log("SUCCESS!");
-        }
-    });   
+    const text = response.text;
+    const textColor = response.textColor;
+    const shapeType = response.shapeType;
+    const shapeColor = response.shapeColor;
+
+    generateShapes(text,textColor,shapeType,shapeColor);
 })
 .catch((err) => console.log(err));
+
+function generateShapes(text, textColor, shapeType, shapeColor) {
+    if (shapeType === 'Triangle') {
+        const triangle = new Triangle(text, textColor, shapeColor)
+        return fs.writeFile('logo.svg',triangle,(err) => {
+            if(err) {
+                console.log(err);
+            }
+            else {
+                console.log("SUCCESS!");
+            }
+        });   
+       
+    }
+
+    if (shapeType === 'Circle') {
+        const circle = new Circle(text, textColor, shapeColor)
+        return `<svg width="300" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="150" cy="100" r="100" fill="${this.shapeColor}" />
+            <text x="150" y="125" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+        </svg> `
+    }
+
+    if (shapeType === 'Square') {
+        const square = new Square(text, textColor, shapeColor)
+        return `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <rect width="200" height="200" fill="${this.shapeColor}"/>
+            <text x="100" y="125" font-size="70" text-anchor="middle" fill="${this.textColor}">${this.text}</text>
+        </svg>`
+    }
+
+}
+
